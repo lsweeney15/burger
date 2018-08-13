d6925031fd941f41cc69d695e1d166b1cb5e14a7
Liam Sweeney <liamsweeney@liam-sweeneys-computer.local>
2018-08-13 00:50:47 -0400

    initial commit

---
+++ models/burger.js
@@ -0,0 +1,11 @@
+var orm = require('../confi/orm.js');
+
+var burger = {
+    all: function(cb){
+        orm.all("burgers", function(res){
+cb(res);
+        })
+    }
+}
+
+module.exports = burger;
